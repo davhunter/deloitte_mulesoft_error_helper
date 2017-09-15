558dbf1492a89b1a6dc0293f67b986fda40dd1ab
davhunter <davhunter@deloitte.ca>
2017-09-15 14:29:41 -0400

    final version that works, with javadoc

--- src/main/java/com/client/application/exceptions/ManualErrorException.java
+++ src/main/java/com/client/application/exceptions/ManualErrorException.java
@@ -1,5 +1,8 @@
 package com.client.application.exceptions;
 
+/**
+ * Exception caused by manual intervention
+ */
 public class ManualErrorException extends Exception {
 
 	public ManualErrorException(String message) {
@@ -7,6 +10,5 @@ public class ManualErrorException extends Exception {
 	}
 
 	private static final long serialVersionUID = 1L;
-
 
 }
---
+++ src/main/java/com/client/application/exceptions/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains Exceptions to be thrown by the Error Handling Tester
+ * application
+ */
+package com.client.application.exceptions;
