b05a523d8387d2541f0ccbcfe9eb9b985c70b99a
davhunter <davhunter@deloitte.ca>
2017-09-15 14:09:16 -0400

    fixed error class implementation

--- src/main/java/com/client/application/exceptions/ManualErrorException.java
+++ src/main/java/com/client/application/exceptions/ManualErrorException.java
@@ -4,10 +4,11 @@ import org.mule.module.apikit.exception.BadRequestException;
 
 public class ManualErrorException extends BadRequestException {
 
+	public ManualErrorException(String message) {
+		super(message);
+	}
+
 	private static final long serialVersionUID = 1L;
 
-	public ManualErrorException(Throwable e) {
-		super(e);
-	}
 
 }
