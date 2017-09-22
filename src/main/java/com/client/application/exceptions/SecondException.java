a1ab15d480cdcfdb19b37119a9206a396e6573ca
davhunter <davhunter@deloitte.ca>
2017-09-22 15:08:13 -0400

    added SecondException

---
+++ src/main/java/com/client/application/exceptions/SecondException.java
@@ -0,0 +1,11 @@
+package com.client.application.exceptions;
+
+public class SecondException extends Exception {
+
+	public SecondException(String message) {
+		super(message);
+	}
+	
+	private static final long serialVersionUID = 1L;
+
+}
