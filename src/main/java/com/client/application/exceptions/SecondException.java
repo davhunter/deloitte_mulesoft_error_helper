3b33142de463c2c03c95bf13d66b40f9b828ede8
davhunter <davhunter@deloitte.ca>
2017-09-23 13:50:11 -0400

    small change to javadoc

--- src/main/java/com/client/application/exceptions/SecondException.java
+++ src/main/java/com/client/application/exceptions/SecondException.java
@@ -1,11 +1,14 @@
 package com.client.application.exceptions;
 
+/**
+ * Another exception that could be used for testing the error handling code
+ */
 public class SecondException extends Exception {
 
 	public SecondException(String message) {
 		super(message);
 	}
-	
+
 	private static final long serialVersionUID = 1L;
 
 }
